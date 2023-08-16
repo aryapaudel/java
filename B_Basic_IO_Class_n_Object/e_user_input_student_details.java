533d17ab4414cc09d04ca0b2f08ae46f32355e50
mallabiju <mallabiju@gmail.com>
2023-08-16 06:59:33 +0545

    student details

---
+++ B_Basic_IO_Class_n_Object/e_user_input_student_details.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class Student {
+    int roll;
+    int age;
+    String name;
+
+    void getdata() {
+        Scanner sc = new Scanner(System.in);
+        roll = sc.nextInt();
+        age = sc.nextInt();
+        name = sc.next();
+        // sc.close();
+    }
+
+    void display() {
+        System.out.println("Roll =" + roll);
+        System.out.println("Age =" + age);
+        System.out.println("Name =" + name);
+    }
+}
+
+public class e_user_input_student_details {
+    public static void main(String[] args) {
+        Student st1 = new Student();
+        Student st2 = new Student();
+        System.out.println("Enter the details of the student1:");
+        st1.getdata();
+        System.out.println("Enter the details of the student2:");
+        st2.getdata();
+        System.out.println("Details of the student1:");
+        st1.display();
+        System.out.println("Details of the student2:");
+        st2.display();
+    }
+}
