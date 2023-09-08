579f111ba3b0d9944a5b67d3bd0505352118cfc0
mallabiju <mallabiju@gmail.com>
2023-09-08 08:09:01 +0545

    Operators

---
+++ D_Operators/j_increment.java
@@ -0,0 +1,9 @@
+package D_Operators;
+
+public class j_increment {
+    public static void main(String[] args) {
+        int a = 5;
+        // System.out.println(a++); // Post-increment // 5
+        System.out.println(++a); // Pre-increment // 6
+    }
+}
---
+++ D_Operators/k_decrement.java
@@ -0,0 +1,9 @@
+package D_Operators;
+
+public class k_decrement {
+    public static void main(String[] args) {
+        int a = 5;
+        // System.out.println(a--); // Post-decrement // 5
+        System.out.println(--a); // Pre-decrement // 4
+    }
+}
